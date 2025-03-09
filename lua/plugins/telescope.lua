0ad69ea37daedea598f56b9a288ab6204db5f2a7
Johann Tveranger <johann.tveranger@gmail.com>
2025-03-09 13:02:27 +0100

    feat/Add plugin telescope-ui-select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,22 +1,26 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.5',
-	dependencies = { 'nvim-lua/plenary.nvim',
-		'sharkdp/fd' },
-	config = function()
-		require('telescope').setup {
-			defaults = {
-				path_display = { "smart" },
-				mappings = {
-					["i"] = {
-					["<C-j>"] = require('telescope.actions').move_selection_next,
-					["<C-k>"] = require('telescope.actions').move_selection_previous,
-					}
-				},
+	{
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim',
+			'sharkdp/fd' },
+		config = function()
+			require('telescope').setup {
+				defaults = {
+					path_display = { "smart" },
+					mappings = {
+						["i"] = {
+							["<C-j>"] = require('telescope.actions').move_selection_next,
+							["<C-k>"] = require('telescope.actions').move_selection_previous,
+						}
+					},
+				}
 			}
-		}
 
-		require("telescope").load_extension("file_browser")
-
-	end
+			require("telescope").load_extension("file_browser")
+			require("telescope").load_extension("ui-select")
+		end
+	},
+	-- Replaces core nvim menus with telescope because we love telescope
+	{ 'nvim-telescope/telescope-ui-select.nvim' }
 }
